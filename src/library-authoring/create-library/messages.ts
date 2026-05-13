9b8222521eb96e4632f61a2648ad644d9f8c42da
Jacob Puc <jacobesa619@gmail.com>
2026-05-13 10:54:08 -0600

    fix: traduce apartado library

--- src/library-authoring/create-library/messages.ts
+++ src/library-authoring/create-library/messages.ts
@@ -3,89 +3,93 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   createLibrary: {
     id: 'course-authoring.library-authoring.create-library',
-    defaultMessage: 'Create new library',
+    defaultMessage: 'Crear nueva biblioteca',
     description: 'Header for the create library form',
   },
   titleLabel: {
     id: 'course-authoring.library-authoring.create-library.form.title.label',
-    defaultMessage: 'Library name',
+    defaultMessage: 'Nombre de la biblioteca',
     description: 'Label for the title field.',
   },
   titlePlaceholder: {
     id: 'course-authoring.library-authoring.create-library.form.title.placeholder',
-    defaultMessage: 'e.g. Computer Science Problems',
+    defaultMessage: 'ej. Problemas de informática',
     description: 'Placeholder text for the title field.',
   },
   titleHelp: {
     id: 'course-authoring.library-authoring.create-library.form.title.help',
-    defaultMessage: 'The name for your library',
+    defaultMessage: 'El nombre de tu biblioteca',
     description: 'Help text for the title field.',
   },
   orgLabel: {
     id: 'course-authoring.library-authoring.create-library.form.org.label',
-    defaultMessage: 'Organization',
+    defaultMessage: 'Organización',
     description: 'Label for the organization field.',
   },
   orgPlaceholder: {
     id: 'course-authoring.library-authoring.create-library.form.org.placeholder',
-    defaultMessage: 'e.g. UniversityX or OrganizationX',
+    defaultMessage: 'ej. UniversityX o OrganizationX',
     description: 'Placeholder text for the organization field.',
   },
   orgHelp: {
     id: 'course-authoring.library-authoring.create-library.form.org.help',
-    defaultMessage: 'The public organization name for your library. This cannot be changed.',
+    defaultMessage:
+      'El nombre de la organización pública a la que pertenece su biblioteca. Este nombre no se puede cambiar.',
     description: 'Help text for the organization field.',
   },
   slugLabel: {
     id: 'course-authoring.library-authoring.create-library.form.slug.label',
-    defaultMessage: 'Library ID',
+    defaultMessage: 'ID de la biblioteca',
     description: 'Label for the slug field.',
   },
   slugPlaceholder: {
     id: 'course-authoring.library-authoring.create-library.form.slug.placeholder',
-    defaultMessage: 'e.g. CSPROB',
+    defaultMessage: 'ej. CSPROB',
     description: 'Placeholder text for the slug field.',
   },
   slugHelp: {
     id: 'course-authoring.library-authoring.create-library.form.slug.help',
-    defaultMessage: `The unique code that identifies this library. Note: This is 
-    part of your library URL, so no spaces or special characters are allowed. 
-    This cannot be changed.`,
+    defaultMessage: `El código único que identifica esta biblioteca. Nota: Este es 
+    parte de la URL de tu biblioteca, por lo que no se permiten espacios ni caracteres especiales. 
+    Este valor no se puede cambiar.`,
     description: 'Help text for the slug field.',
   },
   invalidSlugError: {
     id: 'course-authoring.library-authoring.create-library.form.invalid-slug.error',
-    defaultMessage: 'Enter a valid “slug” consisting of Unicode letters, numbers, underscores, or hyphens.',
+    defaultMessage:
+      'Ingrese un “slug” válido compuesto por letras Unicode, números, guiones bajos o guiones.',
     description: 'Text to display when slug id has invalid symbols.',
   },
   requiredFieldError: {
     id: 'course-authoring.library-authoring.create-library.form.required.error',
-    defaultMessage: 'Required field.',
+    defaultMessage: 'Campo requerido.',
     description: 'Error message to display when a required field is missing.',
   },
   disallowedCharsError: {
     id: 'course-authoring.library-authoring.create-library.form.disallowed-chars.error',
-    defaultMessage: 'Please do not use any spaces or special characters in this field.',
-    description: 'Error message to display when a field contains disallowed characters.',
+    defaultMessage:
+      'Por favor, no uses espacios ni caracteres especiales en este campo.',
+    description:
+      'Error message to display when a field contains disallowed characters.',
   },
   noSpaceError: {
     id: 'course-authoring.library-authoring.create-library.form.no-space.error',
-    defaultMessage: 'Please do not use any spaces in this field.',
+    defaultMessage: 'Por favor, no uses espacios en este campo.',
     description: 'Error message to display when a field contains spaces.',
   },
   createLibraryButton: {
     id: 'course-authoring.library-authoring.create-library.form.create-library.button',
-    defaultMessage: 'Create',
-    description: 'Button text for creating a new library.',
+    defaultMessage: 'Crear',
+    description: 'Texto del botón para crear una nueva biblioteca.',
   },
   createLibraryButtonPending: {
     id: 'course-authoring.library-authoring.create-library.form.create-library.button.pending',
-    defaultMessage: 'Creating..',
+    defaultMessage: 'Creando..',
     description: 'Button text while the library is being created.',
   },
   cancelCreateLibraryButton: {
     id: 'course-authoring.library-authoring.create-library.form.create-library.cancel.button',
-    defaultMessage: 'Cancel',
+    defaultMessage: 'Cancelar',
     description: 'Button text to cancel creating a new library.',
   },
 });
--- src/studio-home/tabs-section/messages.ts
+++ src/studio-home/tabs-section/messages.ts
@@ -1,99 +1,100 @@
-import { defineMessages } from "@edx/frontend-platform/i18n";
+import { defineMessages } from '@edx/frontend-platform/i18n';
 
 const messages = defineMessages({
   coursesTabTitle: {
-    id: "course-authoring.studio-home.courses.tab.title",
-    defaultMessage: "Cursos",
+    id: 'course-authoring.studio-home.courses.tab.title',
+    defaultMessage: 'Cursos',
   },
   courseTabErrorMessage: {
-    id: "course-authoring.studio-home.courses.tab.error.message",
+    id: 'course-authoring.studio-home.courses.tab.error.message',
     defaultMessage:
-      "No se pudieron obtener los cursos. Inténtelo de nuevo más tarde..",
+      'No se pudieron obtener los cursos. Inténtelo de nuevo más tarde..',
   },
   coursesPaginationInfo: {
-    id: "course-authoring.studio-home.courses.pagination.info",
-    defaultMessage: "Mostrando {length} de {total}",
+    id: 'course-authoring.studio-home.courses.pagination.info',
+    defaultMessage: 'Mostrando {length} de {total}',
   },
   librariesTabErrorMessage: {
-    id: "course-authoring.studio-home.libraries.tab.error.message",
+    id: 'course-authoring.studio-home.libraries.tab.error.message',
     defaultMessage:
-      "No se pudieron obtener las bibliotecas. Inténtelo de nuevo más tarde.",
+      'No se pudieron obtener las bibliotecas. Inténtelo de nuevo más tarde.',
   },
   librariesTabTitle: {
-    id: "course-authoring.studio-home.libraries.tab.title",
-    defaultMessage: "Bibliotecas",
+    id: 'course-authoring.studio-home.libraries.tab.title',
+    defaultMessage: 'Bibliotecas',
   },
   legacyLibrariesTabTitle: {
-    id: "course-authoring.studio-home.legacy.libraries.tab.title",
-    defaultMessage: "Bibliotecas heredadas",
+    id: 'course-authoring.studio-home.legacy.libraries.tab.title',
+    defaultMessage: 'Bibliotecas heredadas',
   },
   archivedTabTitle: {
-    id: "course-authoring.studio-home.archived.tab.title",
-    defaultMessage: "Cursos archivados",
+    id: 'course-authoring.studio-home.archived.tab.title',
+    defaultMessage: 'Cursos archivados',
   },
   archiveTabErrorMessage: {
-    id: "course-authoring.studio-home.archived.tab.error.message",
+    id: 'course-authoring.studio-home.archived.tab.error.message',
     defaultMessage:
-      "No se pudieron obtener los cursos archivados. Inténtelo de nuevo más tarde.",
+      'No se pudieron obtener los cursos archivados. Inténtelo de nuevo más tarde.',
   },
   coursesTabCourseNotFoundAlertTitle: {
-    id: "course-authoring.studio-home.courses.tab.course.not.found.alert.title",
-    defaultMessage: "No se encontró ningún resultado",
+    id: 'course-authoring.studio-home.courses.tab.course.not.found.alert.title',
+    defaultMessage: 'No se encontró ningún resultado',
   },
   coursesTabCourseNotFoundAlertMessage: {
-    id: "course-authoring.studio-home.courses.tab.course.not.found.alert.message",
-    defaultMessage: "No hay cursos con los filtros actuales.",
+    id: 'course-authoring.studio-home.courses.tab.course.not.found.alert.message',
+    defaultMessage: 'No hay cursos con los filtros actuales.',
   },
   coursesTabCourseNotFoundAlertCleanFiltersButton: {
-    id: "course-authoring.studio-home.courses.tab.course.not.found.alert.clean.filters.button",
-    defaultMessage: "Limpiar Filtros",
+    id: 'course-authoring.studio-home.courses.tab.course.not.found.alert.clean.filters.button',
+    defaultMessage: 'Limpiar Filtros',
   },
   taxonomiesTabTitle: {
-    id: "course-authoring.studio-home.taxonomies.tab.title",
-    defaultMessage: "Taxonomías",
-    description: "Título de la pestaña Taxonomías en la página principal",
+    id: 'course-authoring.studio-home.taxonomies.tab.title',
+    defaultMessage: 'Taxonomías',
+    description: 'Título de la pestaña Taxonomías en la página principal',
   },
   libraryV2PlaceholderTitle: {
-    id: "course-authoring.studio-home.libraries.placeholder.title",
-    defaultMessage: "Library V2 Placeholder",
+    id: 'course-authoring.studio-home.libraries.placeholder.title',
+    defaultMessage: 'Library V2 Placeholder',
   },
   libraryV2PlaceholderBody: {
-    id: "course-authoring.studio-home.libraries.placeholder.body",
+    id: 'course-authoring.studio-home.libraries.placeholder.body',
     defaultMessage:
-      "This is a placeholder page, as the Library Authoring MFE is not enabled.",
+      'Esta es una página provisional, ya que la función MFE de creación de bibliotecas no está habilitada.',
   },
   librariesV2TabBetaBadge: {
-    id: "course-authoring.studio-home.libraries.tab.library.beta-badge",
-    defaultMessage: "Beta",
-    description: 'Text used to mark the Libraries v2 feature as "in beta"',
+    id: 'course-authoring.studio-home.libraries.tab.library.beta-badge',
+    defaultMessage: 'Beta',
+    description:
+      'Texto utilizado para marcar la función Bibliotecas v2 como "en versión beta".',
   },
   librariesV2TabBetaText: {
-    id: "course-authoring.studio-home.libraries.tab.library.beta-text",
+    id: 'course-authoring.studio-home.libraries.tab.library.beta-text',
     defaultMessage:
-      "¡Bienvenidos a la nueva experiencia de Bibliotecas Beta! Las Bibliotecas se han rediseñado por completo," +
-      " lo que facilita enormemente la reutilización y la remezcla del contenido del curso. El nuevo espacio de Bibliotecas te permite crear, organizar y" +
-      " administrar contenido nuevo; reutilizar tu contenido en tantos cursos como quieras; sincronizar las actualizaciones de forma centralizada; y crear" +
-      " conjuntos de problemas aleatorios. Consulta {link} para obtener más detalles..",
+      '¡Bienvenidos a la nueva experiencia de Bibliotecas Beta! Las Bibliotecas se han rediseñado por completo,' +
+      ' lo que facilita enormemente la reutilización y la remezcla del contenido del curso. El nuevo espacio de Bibliotecas te permite crear, organizar y' +
+      ' administrar contenido nuevo; reutilizar tu contenido en tantos cursos como quieras; sincronizar las actualizaciones de forma centralizada; y crear' +
+      ' conjuntos de problemas aleatorios. Consulta {link} para obtener más detalles.',
     description:
-      "Explanatory text shown on the Libraries v2 tab during the beta release.",
+      'Texto explicativo mostrado en la pestaña Bibliotecas v2 durante la versión beta.',
   },
   librariesV2TabBetaTutorialLinkText: {
-    id: "course-authoring.studio-home.libraries.tab.library.beta-link-text",
-    defaultMessage: "Tutorial de bibliotecas v2",
+    id: 'course-authoring.studio-home.libraries.tab.library.beta-link-text',
+    defaultMessage: 'Tutorial de bibliotecas v2',
     description:
-      'Text to use as the link in the "course-authoring.studio-home.libraries.tab.library.beta-text" message',
+      'Texto a usar como enlace en el mensaje "course-authoring.studio-home.libraries.tab.library.beta-text"',
   },
   librariesV2TabLibrarySearchPlaceholder: {
-    id: "course-authoring.studio-home.libraries.tab.library.search-placeholder",
-    defaultMessage: "Buscar",
+    id: 'course-authoring.studio-home.libraries.tab.library.search-placeholder',
+    defaultMessage: 'Buscar',
   },
   librariesV2TabLibraryNotFoundAlertTitle: {
-    id: "course-authoring.studio-home.libraries.tab.library.not.found.alert.title",
-    defaultMessage: "We could not find any result",
+    id: 'course-authoring.studio-home.libraries.tab.library.not.found.alert.title',
+    defaultMessage: 'No pudimos encontrar ningún resultado.',
   },
   librariesV2TabLibraryNotFoundAlertMessage: {
-    id: "course-authoring.studio-home.libraries.tab.library.not.found.alert.message",
-    defaultMessage: "There are no libraries with the current filters.",
+    id: 'course-authoring.studio-home.libraries.tab.library.not.found.alert.message',
+    defaultMessage: 'No hay bibliotecas con los filtros actuales.',
   },
 });
 
