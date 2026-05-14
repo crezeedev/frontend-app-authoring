36902c7b368f3a922b941122108f0ae9f5abf70a
LayeskaHaas <akseyalhaas10@gmail.com>
2026-05-14 12:21:22 -0600

    fix: Traducción de card progreso

--- plugins/course-apps/progress/messages.js
+++ plugins/course-apps/progress/messages.js
@@ -3,30 +3,30 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   heading: {
     id: 'course-authoring.pages-resources.progress.heading',
-    defaultMessage: 'Configure progress',
+    defaultMessage: 'Configurar el progreso',
   },
   enableProgressLabel: {
     id: 'course-authoring.pages-resources.progress.enable-progress.label',
-    defaultMessage: 'Progress',
+    defaultMessage: 'Progreso',
   },
   enableProgressHelp: {
     id: 'course-authoring.pages-resources.progress.enable-progress.help',
-    defaultMessage: `As students work through graded assignments, scores
-        will appear under the progress tab. The progress tab contains a chart of
-        all graded assignments in the course, with a list of all assignments and
-        scores below.`,
+    defaultMessage: `A medida que los estudiantes completen las tareas 
+    calificadas, las puntuaciones aparecerán en la pestaña de progreso. 
+    Esta pestaña contiene una tabla con todas las tareas calificadas 
+    del curso, junto con una lista de todas las tareas y sus puntuaciones.`,
   },
   enableProgressLink: {
     id: 'course-authoring.pages-resources.progress.enable-progress.link',
-    defaultMessage: 'Learn more about progress',
+    defaultMessage: 'Obtenga más información sobre el progreso.',
   },
   enableGraphLabel: {
     id: 'course-authoring.pages-resources.progress.enable-graph.label',
-    defaultMessage: 'Enable progress graph',
+    defaultMessage: 'Habilitar gráfico de progreso',
   },
   enableGraphHelp: {
     id: 'course-authoring.pages-resources.progress.enable-graph.help',
-    defaultMessage: 'If enabled, students can view the progress graph',
+    defaultMessage: 'Si está habilitado, los estudiantes pueden ver el gráfico de progreso.',
   },
 });
 
