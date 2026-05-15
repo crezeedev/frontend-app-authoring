e711a5a0078853845e348bde227665eeb44dd291
Jacob Puc <jacobesa619@gmail.com>
2026-05-15 11:52:37 -0600

    fix: corrige traduccion repetir curso

--- src/studio-home/messages.ts
+++ src/studio-home/messages.ts
@@ -1,76 +1,76 @@
-import { defineMessages } from "@edx/frontend-platform/i18n";
+import { defineMessages } from '@edx/frontend-platform/i18n';
 
 const messages = defineMessages({
   headingTitle: {
-    id: "course-authoring.studio-home.heading.title",
-    defaultMessage: "{studioShortName} home",
+    id: 'course-authoring.studio-home.heading.title',
+    defaultMessage: '{studioShortName} home',
   },
   addNewCourseBtnText: {
-    id: "course-authoring.studio-home.add-new-course.btn.text",
-    defaultMessage: "Nuevo Curso",
+    id: 'course-authoring.studio-home.add-new-course.btn.text',
+    defaultMessage: 'Nuevo Curso',
   },
   addNewLibraryBtnText: {
-    id: "course-authoring.studio-home.add-new-library.btn.text",
-    defaultMessage: "Nueva biblioteca",
+    id: 'course-authoring.studio-home.add-new-library.btn.text',
+    defaultMessage: 'Nueva biblioteca',
   },
   homePageLoadFailedMessage: {
-    id: "course-authoring.studio-home.page-load.failed.message",
+    id: 'course-authoring.studio-home.page-load.failed.message',
     defaultMessage:
-      "No se pudo cargar la página principal de Studio. Inténtalo de nuevo más tarde.",
+      'No se pudo cargar la página principal de Studio. Inténtalo de nuevo más tarde.',
   },
   emailStaffBtnText: {
-    id: "course-authoring.studio-home.email-staff.btn.text",
-    defaultMessage: "Email staff to create course",
+    id: 'course-authoring.studio-home.email-staff.btn.text',
+    defaultMessage: 'Email staff to create course',
   },
   defaultSection_1_Title: {
-    id: "course-authoring.studio-home.default-section-1.title",
+    id: 'course-authoring.studio-home.default-section-1.title',
     defaultMessage:
-      "¿Eres parte del equipo de un curso existente en {studioShortName}?",
+      '¿Eres parte del equipo de un curso existente en {studioShortName}?',
   },
   defaultSection_1_Description: {
-    id: "course-authoring.studio-home.default-section-1.description",
+    id: 'course-authoring.studio-home.default-section-1.description',
     defaultMessage:
-      "El creador del curso debe darte acceso al curso. Contacta con el creador del curso o con el administrador del curso que estás ayudando a crear.",
+      'El creador del curso debe darte acceso al curso. Contacta con el creador del curso o con el administrador del curso que estás ayudando a crear.',
   },
   defaultSection_2_Title: {
-    id: "course-authoring.studio-home.default-section-2.title",
-    defaultMessage: "Crear tu primer curso",
+    id: 'course-authoring.studio-home.default-section-2.title',
+    defaultMessage: 'Crear tu primer curso',
   },
   defaultSection_2_Description: {
-    id: "course-authoring.studio-home.default-section-2.description",
-    defaultMessage: "¡Tu nuevo curso está a solo un clic!",
+    id: 'course-authoring.studio-home.default-section-2.description',
+    defaultMessage: '¡Tu nuevo curso está a solo un clic!',
   },
   btnAddNewCourseText: {
-    id: "course-authoring.studio-home.btn.add-new-course.text",
-    defaultMessage: "Crear tu primer curso",
+    id: 'course-authoring.studio-home.btn.add-new-course.text',
+    defaultMessage: 'Crear tu primer curso',
   },
   btnReRunText: {
-    id: "course-authoring.studio-home.btn.re-run.text",
-    defaultMessage: "Repetir curso",
+    id: 'course-authoring.studio-home.btn.re-run.text',
+    defaultMessage: 'Relanzar curso',
   },
   viewLiveBtnText: {
-    id: "course-authoring.studio-home.btn.view-live.text",
-    defaultMessage: "Ver en vivo",
+    id: 'course-authoring.studio-home.btn.view-live.text',
+    defaultMessage: 'Ver en vivo',
   },
   organizationTitle: {
-    id: "course-authoring.studio-home.organization.title",
-    defaultMessage: "Configuración de organización y biblioteca",
+    id: 'course-authoring.studio-home.organization.title',
+    defaultMessage: 'Configuración de organización y biblioteca',
   },
   organizationLabel: {
-    id: "course-authoring.studio-home.organization.label",
-    defaultMessage: "Mostrar todos los cursos en la organización:",
+    id: 'course-authoring.studio-home.organization.label',
+    defaultMessage: 'Mostrar todos los cursos en la organización:',
   },
   organizationSubmitBtnText: {
-    id: "course-authoring.studio-home.organization.btn.submit.text",
-    defaultMessage: "Enviar",
+    id: 'course-authoring.studio-home.organization.btn.submit.text',
+    defaultMessage: 'Enviar',
   },
   organizationInputPlaceholder: {
-    id: "course-authoring.studio-home.organization.input.placeholder",
-    defaultMessage: "Por ejemplo, MITx",
+    id: 'course-authoring.studio-home.organization.input.placeholder',
+    defaultMessage: 'Por ejemplo, MITx',
   },
   organizationInputNoOptions: {
-    id: "course-authoring.studio-home.organization.input.no-options",
-    defaultMessage: "No hay opciones",
+    id: 'course-authoring.studio-home.organization.input.no-options',
+    defaultMessage: 'No hay opciones',
   },
 });
 
