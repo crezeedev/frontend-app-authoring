8edda5d6dc0754fa1a7333af467e12cc408dbfb1
Thaily Cabrera <thailycabrera886@gmail.com>
2026-05-18 14:57:00 -0600

    fix: Traducción en video 'No transcripts added'

--- src/editors/containers/VideoEditor/components/VideoSettingsModal/components/messages.js
+++ src/editors/containers/VideoEditor/components/VideoSettingsModal/components/messages.js
@@ -4,55 +4,55 @@ const messages = defineMessages({
 
   expandAltText: {
     id: 'authoring.videoeditor.expand',
-    defaultMessage: 'Expand',
+    defaultMessage: 'Expandir',
   },
   collapseAltText: {
     id: 'authoring.videoeditor.collapse',
-    defaultMessage: 'Collapse',
+    defaultMessage: 'Colapsar',
   },
   validateErrorTitle: {
     id: 'authoring.videoeditor.validate.error.title',
-    defaultMessage: 'We couldn\'t add your video.',
+    defaultMessage: 'No pudimos agregar tu video.',
     description: 'Title of validation error.',
   },
   validateErrorBody: {
     id: 'authoring.videoeditor.validate.error.body',
-    defaultMessage: 'Please check your entries and try again.',
+    defaultMessage: 'Por favor, revisa tus entradas e inténtalo de nuevo.',
     description: 'Body of validation error.',
   },
   durationTitle: {
     id: 'authoring.videoeditor.duration.title',
-    defaultMessage: 'Duration',
+    defaultMessage: 'Duración',
     description: 'Title of Duration widget',
   },
   durationDescription: {
     id: 'authoring.videoeditor.duration.description',
-    defaultMessage: 'Set a specific section of the video to play.',
+    defaultMessage: 'Establece una sección específica del video para reproducir.',
     description: 'Description of Duration widget',
   },
   startTimeLabel: {
     id: 'authoring.videoeditor.duration.startTime.label',
-    defaultMessage: 'Start time',
+    defaultMessage: 'Hora de inicio',
     description: 'Label of start time input field',
   },
   stopTimeLabel: {
     id: 'authoring.videoeditor.duration.stopTime.label',
-    defaultMessage: 'Stop time',
+    defaultMessage: 'Hora de finalización',
     description: 'Label of stop time input field',
   },
   durationHint: {
     id: 'authoring.videoeditor.duration.hint',
-    defaultMessage: 'Enter time as HH:MM:SS',
+    defaultMessage: 'Ingresa el tiempo en formato HH:MM:SS',
     description: 'Hint text for start and stop time input fields',
   },
   fullVideoLength: {
     id: 'authoring.videoeditor.duration.fullVideoLength',
-    defaultMessage: 'Full video length',
+    defaultMessage: 'Longitud completa del video',
     description: 'Text describing a video with neither custom start time nor custom stop time',
   },
   startsAt: {
     id: 'authoring.videoeditor.duration.startsAt',
-    defaultMessage: 'Starts at {startTime}',
+    defaultMessage: 'Comienza a las {startTime}',
     description: 'Text describing a video with custom start time and default stop time',
   },
   total: {
@@ -62,22 +62,22 @@ const messages = defineMessages({
   },
   custom: {
     id: 'authoring.videoeditor.duration.custom',
-    defaultMessage: 'Custom: {total}',
+    defaultMessage: 'Personalizado: {total}',
     description: 'Text describing a video with custom start time and custom stop time, or just a custom stop time for a collapsed widget',
   },
   noTranscriptsAdded: {
     id: 'authoring.videoeditor.transcripts.empty',
-    defaultMessage: 'No transcripts added',
+    defaultMessage: 'No se añadieron transcripciones',
     description: 'Message shown when the user has not selected any transcripts for the video.',
   },
   videoTypeYoutube: {
     id: 'authoring.videoeditor.videotype.youtube',
-    defaultMessage: 'YouTube video',
+    defaultMessage: 'Video de YouTube',
     description: 'Shown on the preview card if the video is from youtube.com.',
   },
   videoTypeOther: {
     id: 'authoring.videoeditor.videotype.other',
-    defaultMessage: 'Other video',
+    defaultMessage: 'Otro video',
     description: 'Shown on the preview card if the video source could not be identified.',
   },
 });
