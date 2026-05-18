5c5dafa1ab69b29cd5b52e3f7290be629d36d6c8
Thaily Cabrera <thailycabrera886@gmail.com>
2026-05-18 17:59:34 -0600

    fix: Traducción en video 'No transcripts added' pt2

--- src/editors/containers/VideoEditor/components/VideoSettingsModal/components/HandoutWidget/messages.js
+++ src/editors/containers/VideoEditor/components/VideoSettingsModal/components/HandoutWidget/messages.js
@@ -4,48 +4,48 @@ const messages = defineMessages({
 
   titleLabel: {
     id: 'authoring.videoeditor.handout.title.label',
-    defaultMessage: 'Handout',
+    defaultMessage: 'Folleto',
     description: 'Title for the handout widget',
   },
   uploadButtonLabel: {
     id: 'authoring.videoeditor.handout.upload.label',
-    defaultMessage: 'Upload Handout',
+    defaultMessage: 'Subir folleto',
     description: 'Label for upload button',
   },
   addHandoutMessage: {
     id: 'authoring.videoeditor.handout.upload.addHandoutMessage',
-    defaultMessage: `Add a handout to accompany this video. Learners can download
-    this file by clicking "Download Handout" below the video.`,
+    defaultMessage: `Agrega un folleto para acompañar este video.
+    Los alumnos pueden descargar este archivo haciendo clic en "Descargar folleto" debajo del video.`,
     description: 'Message displayed when uploading a handout',
   },
   uploadHandoutError: {
     id: 'authoring.videoeditor.handout.error.uploadHandoutError',
-    defaultMessage: 'Failed to upload handout. Please try again.',
+    defaultMessage: 'No se pudo subir el folleto. Por favor, inténtalo de nuevo.',
     description: 'Message presented to user when handout fails to upload',
   },
   fileSizeError: {
     id: 'authoring.videoeditor.handout.error.fileSizeError',
-    defaultMessage: 'Handout files must be 20 MB or less. Please resize the file and try again.',
+    defaultMessage: 'Los archivos de folleto deben ser de 20 MB o menos. Por favor, redimensiona el archivo e inténtalo de nuevo.',
     description: 'Message presented to user when handout file size is larger than 20 MB',
   },
   handoutHelpMessage: {
     id: 'authoring.videoeditor.handout.handoutHelpMessage',
-    defaultMessage: 'Learners can download this file by clicking "Download Handout" below the video.',
+    defaultMessage: 'Los alumnos pueden descargar este archivo haciendo clic en "Descargar folleto" debajo del video.',
     description: 'Message presented to user when a handout is present',
   },
   deleteHandout: {
     id: 'authoring.videoeditor.handout.deleteHandout',
-    defaultMessage: 'Delete',
+    defaultMessage: 'Eliminar',
     description: 'Message Presented To user for action to delete handout',
   },
   replaceHandout: {
     id: 'authoring.videoeditor.handout.replaceHandout',
-    defaultMessage: 'Replace',
+    defaultMessage: 'Reemplazar',
     description: 'Message Presented To user for action to replace handout',
   },
   downloadHandout: {
     id: 'authoring.videoeditor.handout.downloadHandout',
-    defaultMessage: 'Download',
+    defaultMessage: 'Descargar',
     description: 'Message Presented To user for action to download handout',
   },
 });
