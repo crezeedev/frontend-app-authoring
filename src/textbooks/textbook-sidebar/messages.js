56f5d4e38f2044e5b1dbe0b8c72e7311e948f121
LayeskaHaas <akseyalhaas10@gmail.com>
2026-05-19 08:42:01 -0600

    fix: Traducción de TextBooks

--- src/textbooks/textbook-sidebar/messages.js
+++ src/textbooks/textbook-sidebar/messages.js
@@ -1,27 +1,27 @@
 const descriptions = {
   section_1_title: {
     id: 'course-authoring.textbooks.sidebar.section-1.title',
-    defaultMessage: 'Why should I break my textbook into chapters?',
+    defaultMessage: '¿Por qué debería dividir mi libro de texto en capítulos?',
     description: 'Title for section 1 in the textbooks sidebar',
   },
   section_1_descriptions: {
     id: 'course-authoring.textbooks.sidebar.section-1.descriptions',
-    defaultMessage: 'Breaking your textbook into multiple chapters reduces loading times for students, especially those with slow Internet connections. Breaking up textbooks into chapters can also help students more easily find topic-based information.',
+    defaultMessage: 'Dividir el libro de texto en varios capítulos reduce los tiempos de carga para los estudiantes, especialmente para aquellos con conexiones a internet lentas. Además, esta división facilita la búsqueda de información sobre temas específicos.',
     description: 'Description for section 1 in the textbooks sidebar',
   },
   section_2_title: {
     id: 'course-authoring.textbooks.sidebar.section-2.title',
-    defaultMessage: 'What if my book isn\'t divided into chapters?',
+    defaultMessage: '¿Qué ocurre si mi libro no está dividido en capítulos?',
     description: 'Title for section 2 in the textbooks sidebar',
   },
   section_2_descriptions: {
     id: 'course-authoring.textbooks.sidebar.section-2.descriptions',
-    defaultMessage: 'If your textbook doesn\'t have individual chapters, you can upload the entire text as a single chapter and enter a name of your choice in the Chapter Name field.',
+    defaultMessage: 'Si su libro de texto no tiene capítulos individuales, puede subir el texto completo como un solo capítulo e introducir el nombre que desee en el campo Nombre del capítulo.',
     description: 'Description for section 2 in the textbooks sidebar',
   },
   sectionLink: {
     id: 'course-authoring.textbooks.sidebar.section-link',
-    defaultMessage: 'Learn more',
+    defaultMessage: 'Más información',
     description: 'Text for the link to learn more in the textbooks sidebar',
   },
 };
