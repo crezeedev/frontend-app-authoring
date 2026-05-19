cd50adbb2b384c4fe8a423230dbb6d343028e0e6
Thaily Cabrera <thailycabrera886@gmail.com>
2026-05-18 18:27:18 -0600

    fix: Traducción en video pt3

--- src/editors/containers/EditorContainer/messages.ts
+++ src/editors/containers/EditorContainer/messages.ts
@@ -4,17 +4,17 @@ const messages = defineMessages({
 
   cancelConfirmTitle: {
     id: 'authoring.editorContainer.cancelConfirm.title',
-    defaultMessage: 'Exit the editor?',
+    defaultMessage: '¿Salir del editor?',
     description: 'Label for modal confirming cancellation',
   },
   cancelConfirmDescription: {
     id: 'authoring.editorContainer.cancelConfirm.description',
-    defaultMessage: 'Are you sure you want to exit the editor? Any unsaved changes will be lost.',
+    defaultMessage: '¿Estás seguro de que quieres salir del editor? Cualquier cambio no guardado se perderá.',
     description: 'Description text for modal confirming cancellation',
   },
   exitButtonAlt: {
     id: 'authoring.editorContainer.exitButton.alt',
-    defaultMessage: 'Exit the editor',
+    defaultMessage: 'Salir del editor',
     description: 'Alt text for the Exit button',
   },
   okButtonLabel: {
@@ -24,32 +24,32 @@ const messages = defineMessages({
   },
   modalTitle: {
     id: 'authoring.editorContainer.accessibleTitle',
-    defaultMessage: 'Editor Dialog',
+    defaultMessage: 'Diálogo del editor',
     description: 'Text that labels the the editor modal dialog for non-visual users',
   },
   contentSaveFailed: {
     id: 'authoring.editorfooter.save.error',
-    defaultMessage: 'Error: Content save failed. Please check recent changes and try again later.',
+    defaultMessage: 'Error: Falló el guardado del contenido. Por favor, revise los cambios recientes y vuelva a intentarlo más tarde.',
     description: 'Error message displayed when content fails to save.',
   },
   cancelButtonAriaLabel: {
     id: 'authoring.editorfooter.cancelButton.ariaLabel',
-    defaultMessage: 'Discard changes and return to learning context',
+    defaultMessage: 'Descartar cambios y volver al contexto de aprendizaje',
     description: 'Screen reader label for cancel button',
   },
   cancelButtonLabel: {
     id: 'authoring.editorfooter.cancelButton.label',
-    defaultMessage: 'Cancel',
+    defaultMessage: 'Cancelar',
     description: 'Label for cancel button',
   },
   saveButtonAriaLabel: {
     id: 'authoring.editorfooter.savebutton.ariaLabel',
-    defaultMessage: 'Save changes and return to learning context',
+    defaultMessage: 'Guardar cambios y volver al contexto de aprendizaje',
     description: 'Screen reader label for save button',
   },
   saveButtonLabel: {
     id: 'authoring.editorfooter.savebutton.label',
-    defaultMessage: 'Save',
+    defaultMessage: 'Guardar',
     description: 'Label for Save button',
   },
 });
