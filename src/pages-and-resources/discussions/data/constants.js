318f28cd779137290494bab55d79a22bc7157859
LayeskaHaas <akseyalhaas10@gmail.com>
2026-05-21 12:53:00 -0600

    feat: Traducción de las Cards Páginas y Recursos

--- src/pages-and-resources/discussions/data/constants.js
+++ src/pages-and-resources/discussions/data/constants.js
@@ -27,9 +27,9 @@ export const discussionRestriction = {
 };
 
 export const discussionRestrictionLabels = {
-  OFF: 'Off',
-  ON: 'On',
-  SCHEDULED: 'Scheduled',
+  OFF: 'Apagado',
+  ON: 'Encendido',
+  SCHEDULED: 'Programado',
 };
 
 export const discussionRestrictionOptions = [
