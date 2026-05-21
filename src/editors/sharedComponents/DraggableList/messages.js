2bdb0c28a665d85d93203f9d37d1eeba33225428
LayeskaHaas <akseyalhaas10@gmail.com>
2026-05-21 13:31:40 -0600

    feat: Traducción de las Cards Páginas y Recursos-Páginas personalizadas

--- src/editors/sharedComponents/DraggableList/messages.js
+++ src/editors/sharedComponents/DraggableList/messages.js
@@ -3,7 +3,7 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   tooltipContent: {
     id: 'authoring.draggableList.tooltip.content',
-    defaultMessage: 'Drag to reorder',
+    defaultMessage: 'Arrastra para reordenar',
     description: 'Tooltip content for drag indicator icon',
   },
 });
