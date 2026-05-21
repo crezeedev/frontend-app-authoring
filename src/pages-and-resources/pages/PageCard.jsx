c6430b38154da85b03927ed91a0d31699a2f853a
LayeskaHaas <akseyalhaas10@gmail.com>
2026-05-21 11:50:18 -0600

    feat: Traducción de las Cards Páginas y Recursos

--- src/pages-and-resources/pages/PageCard.jsx
+++ src/pages-and-resources/pages/PageCard.jsx
@@ -23,11 +23,44 @@ const CoursePageShape = PropTypes.shape({
 export { CoursePageShape };
 
 const translations = {
-  "Allow learners to highlight passages and make notes right in the course.":
-    "Permite a los estudiantes resaltar pasajes y tomar notas directamente en el curso.",
+  // TITLES
+  'Progress': 'Progreso',
+  'Discussion': 'Discusión',
+  'Notes': 'Notas',
+  'Wiki': 'Wiki',
+  'Calculator': 'Calculadora',
+  'Live': 'En vivo',
+  'Textbooks': 'Libros de texto',
+  'Custom pages': 'Páginas personalizadas',
+  'Flexible Peer Grading for ORAs': 'Calificación flexible entre pares para ORAs',
 
-  "Keep learners engaged and on track throughout the course.":
-    "Mantén a los estudiantes comprometidos y al día durante el curso.",
+  // DESCRIPTIONS
+  'Keep learners engaged and on track throughout the course.':
+    'Mantén a los estudiantes comprometidos y al día durante el curso.',
+
+  'Encourage participation and engagement in your course with discussions.':
+    'Fomenta la participación e interacción en tu curso mediante discusiones.',
+
+  'Allow learners to highlight passages and make notes right in the course.':
+    'Permite a los estudiantes resaltar pasajes y tomar notas directamente en el curso.',
+
+  'Enable learners to access, and collaborate on course-related information.':
+    'Permite a los estudiantes acceder y colaborar en información relacionada con el curso.',
+
+  'Provide an in-course calculator for simple and complex calculations.':
+    'Proporciona una calculadora dentro del curso para cálculos simples y complejos.',
+
+  'Enable in-platform video conferencing by configuring live':
+    'Habilita videoconferencias dentro de la plataforma configurando Live.',
+
+  'Create and manage a library of course readings, textbooks, and chapters.':
+    'Crea y administra una biblioteca de lecturas, libros y capítulos del curso.',
+
+  'Provide additional course content and resources with custom pages':
+    'Proporciona contenido y recursos adicionales del curso mediante páginas personalizadas.',
+
+  'Course level settings for Flexible Peer Grading Open Response Assessments.':
+    'Configuraciones a nivel de curso para evaluaciones ORA con calificación flexible entre pares.',
 };
 
 const translate = (text) => translations[text] || text;
@@ -49,7 +82,7 @@ const PageCard = ({
       })}
     >
       <Card.Header
-        title={page.name}
+        title={translate(page.name)}
         subtitle={page.enabled && (
           <Badge variant="success" className="mt-1">
             {formatMessage(messages.enabled)}
